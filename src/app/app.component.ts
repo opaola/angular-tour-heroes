436201f866daab93324acced1f37e71a1b26c978
Andrómeda <128003882+opaola@users.noreply.github.com>
2023-08-01 15:27:21 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,35 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Paola'; /* en el componente angular original se cambia titulo por la variable creada en 'ejemploangular.ts'. y siempre deben ir publico para que coja en el html*/
-  age = 18;
-  img = '/assets/imagen/_a291cbef-3687-4db6-ad9c-1ac05833e437.jpg';
-  btnDisabled = true; /*puedo activar o desactivar propiedades que se renderiza automaticamente en el html*/
-
-   /*objetos*/
-  person = {
-    name: 'Paola',
-    age: 18,
-    avatar: '/assets/imagen/_96cf83a7-88bb-41e7-bd7b-a4f2bcce7de3.jpg',
-  }
-  toggleButton() {
-    this.btnDisabled = !this.btnDisabled; /*el ! sirve para negar una funcion, si esta activo entonces quedara inactivo*/
+  title = 'Tour of Heroes';
 }
-increaseAge() {
-  this.person.age += 1; /*para incrementar 1 cada vez que se escucha el evento (oprime el boton)*/
-}
-
-onScroll(event: Event) {
-  const element = event.target as HTMLElement; /*aqui se especifica de que propiedad hace parte, ej: div es de html*/
-  console.log(element.scrollTop); /*scroll es la barra espaciadora, el evento target es para contar la posicion*/
-}
-changeName(event: Event) {
-  const element = event.target as HTMLInputElement; /*aqui se especifica de que propiedad hace parte, ej: input es una propiedad aparte*/
-  this.person.name = element.value; /*esto permite que se cambie el nombre cada vez que escribimos se cambia*/
-}
-}
-
-
